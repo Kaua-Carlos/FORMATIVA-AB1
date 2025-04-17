60882ba4a742366f0074deb57ec9b30e212430af
Kaua-Carlos <kauacarlosxd@gmail.com>
2025-04-16 21:30:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let display = document.getElementById("display");
-}
+
 function adicionarValor(valor) {
     display.value += valor;
 }
 function surpresa(){
   window.open("https://www.youtube.com/watch?v=M9AIMqOEI0g&ab_channel=Gh0stX709%F0%9F%87%A7%F0%9F%87%B7", "_blank");
+}
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+    }
 }
