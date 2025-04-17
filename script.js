a4cacb97e8e16702d046d654f72a7c35e8dcf80d
Kaua-Carlos <kauacarlosxd@gmail.com>
2025-04-16 21:16:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-function salvarBotao(botao) {
-    
-    const textoDoBotao = botao.innerText;
-
-    document.getElementById('selecionado').innerText = `Você selecionou: ${textoDoBotao}`;
-
-    console.log("Botão selecionado:", textoDoBotao);
+let display = document.getElementById("display");
+}
+function adicionarValor(valor) {
+    display.value += valor;
 }
 function surpresa(){
   window.open("https://www.youtube.com/watch?v=M9AIMqOEI0g&ab_channel=Gh0stX709%F0%9F%87%A7%F0%9F%87%B7", "_blank");
