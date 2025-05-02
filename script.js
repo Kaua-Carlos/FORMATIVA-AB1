5356225165b713f4a617099e1230bc3f0ff648a1
Khyquer Ronaldy <khyquer.oliveira@gmail.com>
2025-05-02 00:49:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function adicionarValor(valor) {
     display.value += valor;
 }
 function surpresa(){
-  window.open("https://www.youtube.com/watch?v=M9AIMqOEI0g&ab_channel=Gh0stX709%F0%9F%87%A7%F0%9F%87%B7", "_blank");
+  alert('Hehehe');
 }
 function calcular() {
     try {
